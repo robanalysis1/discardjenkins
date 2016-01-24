cd986028120e6f003d689cf1da510c729012918c
AdiMac <adamsken1221@gmail.com>
2016-01-24 20:24:03 +0100

    Revert "configurations for macmini"
    This reverts commit e4fe1849ec31ab79c1b668c9d89d7a55b14734a7.

--- src/main/java/org/jenkinsci/test/acceptance/FallbackConfig.java
+++ src/main/java/org/jenkinsci/test/acceptance/FallbackConfig.java
@@ -135,11 +135,7 @@ public class FallbackConfig extends AbstractModule {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                cap.setCapability("browser", "FIREFOX_30_WINDOWS_8_64");
-                cap.setCapability("apikey", "c717c5b3-a307-461e-84ea-1232d44cde89");
-                cap.setCapability("email", "test@testfabrik.com");
-                cap.setCapability("record", false);
-                cap.setCapability("extract", false);
+                cap.setBrowserName(properties.getProperty("browserval"));
 
                 return new SeleniumGridConnection().createWebDriver(cap);
 
